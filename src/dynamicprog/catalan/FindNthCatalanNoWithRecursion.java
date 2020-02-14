21664bb074192b48aea6174cfeafef2627a24c8a
krisparis <christophe.elavumoottil@gmail.com>
2020-02-14 17:32:17 +0100

    Initialize FindNthCatalanNoWithRecursion.java

---
+++ src/dynamicprog/catalan/FindNthCatalanNoWithRecursion.java
@@ -0,0 +1,32 @@
+package dynamicprog.catalan;
+
+/**
+ * <p>
+ * In combinatorial mathematics, the Catalan numbers form a sequence of natural
+ * numbers that occur in various counting problems, often involving
+ * recursively-defined object
+ * </p>
+ *
+ * <p>
+ * The first few Catalan numbers for n = 0, 1, 2, 3, … are 1, 1, 2, 5, 14, 42,
+ * 132, 429, 1430, 4862.
+ * </p>
+ *
+ * <h3>Formula</h3>
+ * <p>
+ * 
+ * <ul>
+ * <li>Cn+1​=C0​Cn​+C1​Cn−1​+⋯+Cn​C0​</li>
+ * <li>C0 = 1</li>
+ * </ul>
+ * 
+ * Cn = (2n)! / ((n + 1)!n!)
+ * 
+ * </p>
+ */
+public class FindNthCatalanNoWithRecursion {
+
+	public static void main(String[] args) {
+	}
+
+}

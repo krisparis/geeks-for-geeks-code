795d45d1f69cb0a2bd0947998c5c734611d788ba
krisparis <christophe.elavumoottil@gmail.com>
2020-03-04 17:52:19 +0100

    Update javadoc

--- src/dynamicprog/binomialcoeff/BinomialCoefficientWithRecursion.java
+++ src/dynamicprog/binomialcoeff/BinomialCoefficientWithRecursion.java
@@ -1,8 +1,17 @@
 package dynamicprog.binomialcoeff;
 
 /**
- * <p>C(n, k) = C(n - 1, k) + C(n - 1, k - 1).</p>
- * <p> C(n, 0) = 1, C(n, n) = 1</p>
+ * <p>
+ * Gives the number of ways, disregarding order, that k objects can be chosen
+ * from among n objects; more formally, the number of k-element subsets (or
+ * k-combinations) of an n-element set.
+ * </p>
+ * <p>
+ * C(n, k) = C(n - 1, k) + C(n - 1, k - 1).
+ * </p>
+ * <p>
+ * C(n, 0) = 1, C(n, n) = 1
+ * </p>
  *
  */
 public class BinomialCoefficientWithRecursion {
